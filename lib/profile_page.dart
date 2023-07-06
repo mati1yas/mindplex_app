6be4b6a9e231575818cb315959910495f6e50a5c
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2023-07-07 00:47:22 +0300

    initial

---
+++ lib/profile_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+  @override
+  State<ProfilePage> createState() {
+    return _ProfilePage();
+  }
+}
+
+class _ProfilePage extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Profile page");
+  }
+}
