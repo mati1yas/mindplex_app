ff6a1cab654b870e3e3af2b0ef0804d2d1c3c793
Matiyas Seifu <matiyassseifu@gmail.com>
2023-07-12 15:31:16 +0300

    feat: implemented network request and data storage in secure storage

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,8 +1,1 @@
-class ApiSerivice {
-  Future<String> loginUser(
-      {required String email,
-      required String password,
-      required String loginType}) async {
-    return "";
-  }
-}
+class ApiSerivice {}
