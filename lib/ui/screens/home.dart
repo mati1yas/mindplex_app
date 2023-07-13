eac38ca18894fe76c5cb62c45c8a65135fdc80a3
Matiyas Seifu <matiyassseifu@gmail.com>
2023-07-13 16:34:46 +0300

    feat: add place holder for home page

---
+++ lib/ui/screens/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mindplex_app/auth/auth.dart';
+
+import '../../auth/auth_controller/auth_controller.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+AuthController authController = Get.put(AuthController());
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Home page"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
