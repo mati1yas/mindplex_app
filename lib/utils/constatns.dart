d59c72bf3f15cfe86c4a521e12e645598f6b7cee
Esubalew <esubalew.a2009@gmail.com>
2023-07-14 09:56:54 +0300

    slash removed from baseurl

--- lib/utils/constatns.dart
+++ lib/utils/constatns.dart
@@ -1,4 +1,4 @@
 class AppUrls {
-  static var baseUrl = "https://staging.mindplex.ai/wp-json/";
+  static var baseUrl = "https://staging.mindplex.ai/wp-json";
   static var loginUrl = '$baseUrl/auth/v1/token';
 }
