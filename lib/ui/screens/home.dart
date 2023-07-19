acadbf0bf79a56d073ddef480b660f0f5cce80e8
Esubalew <esubalew.a2009@gmail.com>
2023-07-19 10:07:50 +0300

    unused import removed

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mindplex_app/auth/auth.dart';
 
 import '../../auth/auth_controller/auth_controller.dart';
 
