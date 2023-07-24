27f981a56d08c4a78549f6e7c1fef204314285c4
Matiyas Seifu <matiyassseifu@gmail.com>
2023-07-24 21:35:04 +0300

    fix: fix issues related to routing and add new routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,17 +1,28 @@
 import 'package:get/get.dart';
+import 'package:mindplex_app/auth/auth.dart';
+import 'package:mindplex_app/main.dart';
 import 'package:mindplex_app/splash_screen.dart';
 import 'package:mindplex_app/ui/screens/home.dart';
 
 import '../profile/profile_page.dart';
 
+final String email = Get.arguments['email'];
+
 class AppRoutes {
   static const String home = '/';
+  static const String authPage = '/auth';
   static const String landingPage = '/landingPage';
-  static const String profilePage = '/profilePage';
 
+  static const String profilePage = '/profilePage';
+  static const String verificationPage = '/verificationPage';
   static final List<GetPage> pages = [
     GetPage(name: home, page: () => SplashScreen()),
-    GetPage(name: landingPage, page: () => const Home()),
-    GetPage(name: profilePage, page: () => const ProfilePage())
+    GetPage(name: authPage, page: () => AuthPage()),
+    GetPage(
+        name: landingPage,
+        page: () => MyHomePage(
+              title: "Mindplex",
+            )),
+    GetPage(name: profilePage, page: () => const ProfilePage()),
   ];
 }
