b08da865105dd879340debf13a561db89a6a19c2
Matiyas Seifu <matiyassseifu@gmail.com>
2023-08-13 13:39:08 +0300

    feat: add reader interaction (like,dislike) buttons

--- lib/blogs/screens/blog_detail_page.dart
+++ lib/blogs/screens/blog_detail_page.dart
@@ -11,111 +11,193 @@ class DetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF0c2b46),
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Container(
-            margin: EdgeInsets.only(top: 100),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      height: 40,
-                      width: 40,
-                      margin: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.green,
-                        image: DecorationImage(
-                          image: NetworkImage(details.authorAvatar ?? ""),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * .40,
-                        margin: EdgeInsets.only(right: 3, top: 30),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              details.authorDisplayName!,
-                              style: const TextStyle(
-                                  fontSize: 10,
-                                  fontWeight: FontWeight.w300,
-                                  fontStyle: FontStyle.normal,
+      appBar: AppBar(
+        toolbarHeight: MediaQuery.of(context).size.height * 0.08,
+        backgroundColor: Color.fromARGB(255, 17, 126, 113),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.82,
+            child: SingleChildScrollView(
+              child: Container(
+                  margin: EdgeInsets.only(top: 100),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.green,
+                              image: DecorationImage(
+                                image: NetworkImage(details.authorAvatar ?? ""),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Container(
+                              width: MediaQuery.of(context).size.width * .40,
+                              margin: EdgeInsets.only(right: 3, top: 30),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    details.authorDisplayName!,
+                                    style: const TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w300,
+                                        fontStyle: FontStyle.normal,
+                                        color: Colors.white),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(
+                                    "It's easier to fool people than to confince them that they've been fooled. I spent the last three years as a design Ethicist It's easier to fool people than to confince them that they've been fooled",
+                                    style: TextStyle(
+                                        fontSize: 10,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w200),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.only(
+                                top: 10, left: 30, right: 30, bottom: 10),
+                            margin: EdgeInsets.only(top: 15),
+                            decoration: const BoxDecoration(
+                                color: Color(0xFF0f3e57),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: const Text(
+                              'Follow',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w200,
                                   color: Colors.white),
                             ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              "It's easier to fool people than to confince them that they've been fooled. I spent the last three years as a design Ethicist It's easier to fool people than to confince them that they've been fooled",
-                              style: TextStyle(
-                                  fontSize: 10,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w200),
-                            )
-                          ],
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: Html(
+                          data: details.content,
+                          style: {
+                            'p': Style(
+                                padding: HtmlPaddings.all(10),
+                                fontWeight: FontWeight.w300,
+                                fontSize: FontSize(17),
+                                color: Colors.white),
+                            'p.has-text-align-center': Style(
+                                alignment: Alignment.centerRight,
+                                padding: HtmlPaddings.only(left: 40, right: 40),
+                                fontWeight: FontWeight.w300,
+                                fontSize: FontSize(17),
+                                color: Colors.white),
+                            'figcaption': Style(
+                                padding: HtmlPaddings.all(10),
+                                alignment: Alignment.topRight,
+                                fontWeight: FontWeight.w300,
+                                fontSize: FontSize(15),
+                                color: Colors.yellow),
+                            'h2': Style(
+                                padding: HtmlPaddings.all(10),
+                                fontWeight: FontWeight.bold,
+                                fontSize: FontSize(22),
+                                color: Colors.white),
+                            'h1': Style(
+                                fontWeight: FontWeight.bold,
+                                fontSize: FontSize(22),
+                                color: Colors.red),
+                            'h3': Style(
+                                fontWeight: FontWeight.bold,
+                                fontSize: FontSize(22),
+                                color: Colors.white)
+                          },
                         ),
                       ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(
-                          top: 10, left: 30, right: 30, bottom: 10),
-                      margin: EdgeInsets.only(top: 15),
-                      decoration: const BoxDecoration(
-                          color: Color(0xFF0f3e57),
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      child: const Text(
-                        'Follow',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w200,
-                            color: Colors.white),
+                    ],
+                  )),
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.07,
+            color: Color.fromARGB(255, 17, 126, 113),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.thumb_up_off_alt_outlined,
+                        color: Colors.white,
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text("Like",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w300))
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
                 ),
                 Container(
-                  child: Html(
-                    data: details.content,
-                    style: {
-                      'p': Style(
-                          padding: HtmlPaddings.all(10),
-                          fontWeight: FontWeight.w300,
-                          fontSize: FontSize(17),
-                          color: Colors.white),
-                      'p.has-text-align-center': Style(
-                          alignment: Alignment.centerRight,
-                          padding: HtmlPaddings.only(left: 40, right: 40),
-                          fontWeight: FontWeight.w300,
-                          fontSize: FontSize(17),
-                          color: Colors.white),
-                      'figcaption': Style(
-                          padding: HtmlPaddings.all(10),
-                          alignment: Alignment.topRight,
-                          fontWeight: FontWeight.w300,
-                          fontSize: FontSize(15),
-                          color: Colors.yellow),
-                      'h2': Style(
-                          padding: HtmlPaddings.all(10),
-                          fontWeight: FontWeight.bold,
-                          fontSize: FontSize(22),
-                          color: Colors.white),
-                      'h1': Style(
-                          fontWeight: FontWeight.bold,
-                          fontSize: FontSize(22),
-                          color: Colors.red),
-                      'h3': Style(
-                          fontWeight: FontWeight.bold,
-                          fontSize: FontSize(22),
-                          color: Colors.white)
-                    },
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.thumb_down_off_alt_outlined,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text("DisLike",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w300))
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.share_outlined,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        "Share",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300),
+                      )
+                    ],
                   ),
                 )
               ],
-            )),
+            ),
+          )
+        ],
       ),
     );
   }
