ad922b445556fd1bbe681b17024d841c2d5165b6
wendecoder <wenonezra258@gmail.com>
2023-08-18 07:24:31 -0700

    fixed api response handling

--- lib/models/blog_model.dart
+++ lib/models/blog_model.dart
@@ -12,55 +12,56 @@ class Blog {
   String? minToRead;
   List<Content>? content;
 
-  Blog(
-      {this.url,
-      this.postTypeFormat,
-      this.thumbnailImage,
-      this.authorUsername,
-      this.authorAvatar,
-      this.authorDisplayName,
-      this.publishedAt,
-      this.postTitle,
-      this.overview,
-      this.likes,
-      this.minToRead,
-      this.content});
+  Blog({
+    this.url,
+    this.postTypeFormat,
+    this.thumbnailImage,
+    this.authorUsername,
+    this.authorAvatar,
+    this.authorDisplayName,
+    this.publishedAt,
+    this.postTitle,
+    this.overview,
+    this.likes,
+    this.minToRead,
+    this.content,
+  });
 
-  Blog.fromJson(Map<String, dynamic> json) {
-    url = json['url'];
-    postTypeFormat = json['post_type_format'];
-    thumbnailImage = json['thumbnail_image'];
-    authorUsername = json['author_username'];
-    authorAvatar = json['author_avatar'];
-    authorDisplayName = json['author_display_name'];
-    publishedAt = json['published_at'];
-    postTitle = json['post_title'];
-    overview = json['overview'];
-    likes = json['likes'];
-    minToRead = json['min_to_read'];
-    if (json['content'] != null) {
-      content = <Content>[];
-      json['content'].forEach((v) {
-        content!.add(new Content.fromJson(v));
-      });
-    }
+  factory Blog.fromJson(Map<String, dynamic> json) {
+    return Blog(
+      url: json['url'],
+      postTypeFormat: json['post_type_format'],
+      thumbnailImage: json['thumbnail_image'],
+      authorUsername: json['author_username'],
+      authorAvatar: json['author_avatar'],
+      authorDisplayName: json['author_display_name'],
+      publishedAt: json['published_at'],
+      postTitle: json['post_title'],
+      overview: json['overview'],
+      likes: json['likes'],
+      minToRead: json['min_to_read'],
+      content: (json['content'] as List<dynamic>)
+          .map((contentJson) => Content.fromJson(contentJson))
+          .toList(),
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['url'] = this.url;
-    data['post_type_format'] = this.postTypeFormat;
-    data['thumbnail_image'] = this.thumbnailImage;
-    data['author_username'] = this.authorUsername;
-    data['author_avatar'] = this.authorAvatar;
-    data['author_display_name'] = this.authorDisplayName;
-    data['published_at'] = this.publishedAt;
-    data['post_title'] = this.postTitle;
-    data['overview'] = this.overview;
-    data['likes'] = this.likes;
-    data['min_to_read'] = this.minToRead;
-    if (this.content != null) {
-      data['content'] = this.content!.map((v) => v.toJson()).toList();
+    final Map<String, dynamic> data = {
+      'url': url,
+      'post_type_format': postTypeFormat,
+      'thumbnail_image': thumbnailImage,
+      'author_username': authorUsername,
+      'author_avatar': authorAvatar,
+      'author_display_name': authorDisplayName,
+      'published_at': publishedAt,
+      'post_title': postTitle,
+      'overview': overview,
+      'likes': likes,
+      'min_to_read': minToRead,
+    };
+    if (content != null) {
+      data['content'] = content!.map((v) => v.toJson()).toList();
     }
     return data;
   }
@@ -68,19 +69,21 @@ class Blog {
 
 class Content {
   String? type;
-  String? content;
+  dynamic content;
 
   Content({this.type, this.content});
 
-  Content.fromJson(Map<String, dynamic> json) {
-    type = json['type'];
-    content = json['content'];
+  factory Content.fromJson(Map<String, dynamic> json) {
+    return Content(
+      type: json['type'],
+      content: json['content'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['type'] = this.type;
-    data['content'] = this.content;
-    return data;
+    return {
+      'type': type,
+      'content': content,
+    };
   }
 }
