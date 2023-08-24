5fe9ca41bab5b47c5f4cfa60cfd333784850aae7
matiyas-seifu-a2sv <matiyas.seifu@a2sv.org>
2023-08-24 19:46:25 +0300

    fix: change blog model to have reactive liked and disliked fields

--- lib/models/blog_model.dart
+++ lib/models/blog_model.dart
@@ -1,3 +1,5 @@
+import 'package:get/get.dart';
+
 class Blog {
   String? slug;
   String? url;
@@ -14,66 +16,76 @@ class Blog {
   int? likes;
   String? minToRead;
   List<Content>? content;
+  RxBool isUserLiked = false.obs;
+  RxBool isUserDisliked = false.obs;
 
-  Blog(
-      {this.slug,
-      this.url,
-      this.postTypeFormat,
-      this.thumbnailImage,
-      this.authorUsername,
-      this.authorAvatar,
-      this.authorBio,
-      this.mpxr,
-      this.authorDisplayName,
-      this.publishedAt,
-      this.postTitle,
-      this.overview,
-      this.likes,
-      this.minToRead,
-      this.content});
+  Blog({
+    this.slug,
+    this.url,
+    this.postTypeFormat,
+    this.thumbnailImage,
+    this.authorUsername,
+    this.authorAvatar,
+    this.authorBio,
+    this.mpxr,
+    this.authorDisplayName,
+    this.publishedAt,
+    this.postTitle,
+    this.overview,
+    this.likes,
+    this.minToRead,
+    this.content,
+    bool? userLiked,
+    bool? userDisliked,
+  }) {
+    isUserLiked.value = userLiked ?? false;
+    isUserDisliked.value = userDisliked ?? false;
+  }
 
-  Blog.fromJson(Map<String, dynamic> json) {
-    slug = json['slug'];
-    url = json['url'];
-    postTypeFormat = json['post_type_format'];
-    thumbnailImage = json['thumbnail_image'];
-    authorUsername = json['author_username'];
-    authorAvatar = json['author_avatar'];
-    authorBio = json['author_bio'];
-    mpxr = json['mpxr'];
-    authorDisplayName = json['author_display_name'];
-    publishedAt = json['published_at'];
-    postTitle = json['post_title'];
-    overview = json['overview'];
-    likes = json['likes'];
-    minToRead = json['min_to_read'];
-    if (json['content'] != null) {
-      content = <Content>[];
-      json['content'].forEach((v) {
-        content!.add(new Content.fromJson(v));
-      });
-    }
+  factory Blog.fromJson(Map<String, dynamic> json) {
+    return Blog(
+      slug: json['slug'],
+      url: json['url'],
+      postTypeFormat: json['post_type_format'],
+      thumbnailImage: json['thumbnail_image'],
+      authorUsername: json['author_username'],
+      authorAvatar: json['author_avatar'],
+      authorBio: json['author_bio'],
+      mpxr: json['mpxr'],
+      authorDisplayName: json['author_display_name'],
+      publishedAt: json['published_at'],
+      postTitle: json['post_title'],
+      overview: json['overview'],
+      likes: json['likes'],
+      minToRead: json['min_to_read'],
+      content: (json['content'] as List<dynamic>?)
+          ?.map((c) => Content.fromJson(c))
+          .toList(),
+      userLiked: json['is_user_liked'],
+      userDisliked: json['is_user_disliked'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['slug'] = this.slug;
-    data['url'] = this.url;
-    data['post_type_format'] = this.postTypeFormat;
-    data['thumbnail_image'] = this.thumbnailImage;
-    data['author_username'] = this.authorUsername;
-    data['author_avatar'] = this.authorAvatar;
-    data['author_bio'] = this.authorBio;
-    data['mpxr'] = this.mpxr;
-    data['author_display_name'] = this.authorDisplayName;
-    data['published_at'] = this.publishedAt;
-    data['post_title'] = this.postTitle;
-    data['overview'] = this.overview;
-    data['likes'] = this.likes;
-    data['min_to_read'] = this.minToRead;
-    if (this.content != null) {
-      data['content'] = this.content!.map((v) => v.toJson()).toList();
-    }
+    final Map<String, dynamic> data = {
+      'slug': slug,
+      'url': url,
+      'post_type_format': postTypeFormat,
+      'thumbnail_image': thumbnailImage,
+      'author_username': authorUsername,
+      'author_avatar': authorAvatar,
+      'author_bio': authorBio,
+      'mpxr': mpxr,
+      'author_display_name': authorDisplayName,
+      'published_at': publishedAt,
+      'post_title': postTitle,
+      'overview': overview,
+      'likes': likes,
+      'min_to_read': minToRead,
+      'content': content?.map((c) => c.toJson()).toList(),
+      'is_user_liked': isUserLiked.value,
+      'is_user_disliked': isUserDisliked.value,
+    };
     return data;
   }
 }
@@ -84,15 +96,18 @@ class Content {
 
   Content({this.type, this.content});
 
-  Content.fromJson(Map<String, dynamic> json) {
-    type = json['type'];
-    content = json['content'];
+  factory Content.fromJson(Map<String, dynamic> json) {
+    return Content(
+      type: json['type'],
+      content: json['content'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['type'] = this.type;
-    data['content'] = this.content;
+    final Map<String, dynamic> data = {
+      'type': type,
+      'content': content,
+    };
     return data;
   }
 }
