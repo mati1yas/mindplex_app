3970a5c72184af81b193bca27eb3dce0616947ff
Dechie <dechu373@protonmail.com>
2023-09-05 13:07:34 +0300

    comment page slight improvement

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer_effect/shimmer_effect.dart';
 
-final mainBackgroundColor = Color.fromARGB(255, 12, 45, 68);
+const mainBackgroundColor = Color.fromARGB(255, 12, 45, 68);
 final blogContainerColor = Color(0xFF103e56);
-final blogWidgetContainerColor = Color(0xFF103e56);
+const blogWidgetContainerColor = Color(0xFF103e56);
 final shimmerEffectBase1 = Color(0xFF6eded0).withOpacity(.3);
 final shimmerEffectBase2 = blogContainerColor.withAlpha(250);
 
 final shimmerEffectHighlight1 = Color(0xFF6eded0);
 final shimmerEffectHighlight2 = blogContainerColor.withAlpha(250);
+
+final commentSectionColor = Color(0xff6eded0);
