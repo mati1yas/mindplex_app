8e6006baacf5842dba714b56fdc76d043dc2c5aa
abrsh-10 <abrhamsisay33@gmail.com>
2023-10-17 12:05:22 +0300

    social links UI corrected and redirect to browser on click of a social link icon

---
+++ lib/models/social_link.dart
@@ -0,0 +1,12 @@
+
+class SocialLink{
+  String? facebookLink;
+  String? linkedinLink;
+  String? twitterLink;
+
+  SocialLink({
+    this.facebookLink,
+    this.linkedinLink,
+    this.twitterLink
+});
+}
