bd27672033824b5bd590c8fa5970ef7f2e04ef36
mati1yas <matiyassseifu@gmail.com>
2023-11-02 15:38:46 +0300

    fix: add navigation to about page

--- lib/blogs/landing_page.dart
+++ lib/blogs/landing_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:get/get.dart';
 import 'package:mindplex_app/blogs/blogs_controller.dart';
 import 'package:mindplex_app/blogs/screens/blog_detail_page.dart';
+import 'package:mindplex_app/mindplex_profile/about/about_mindplex.dart';
 import 'package:mindplex_app/mindplex_profile/moderators/moderators_page.dart';
 import 'package:mindplex_app/utils/colors.dart';
 import 'package:shimmer_effect/shimmer_effect.dart';
@@ -331,6 +332,24 @@ class _LandingPageState extends State<LandingPage> {
                   onTap: () {
                     Navigator.of(context).pop();
                     Get.to(() => ModeratorsPage());
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(
+                    Icons.people_alt_sharp,
+                    size: 25,
+                    color: Colors.white,
+                  ),
+                  title: const Text(
+                    'About Us',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    Get.to(() => AboutMindPlex());
                   },
                 ),
                 SizedBox(
