6b3b65e6ed3a8ac1aaaf6704ec382e8f95cb7fdf
mati1yas <matiyassseifu@gmail.com>
2023-11-08 22:14:34 +0300

    feat: create a new widget and make modification to the existing ones to display user profile about section

--- lib/profile/about_screen.dart
+++ lib/profile/about_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:mindplex_app/profile/user_profile_controller.dart';
 import 'package:mindplex_app/utils/colors.dart';
+
+import 'widgets/user_interest_widget.dart';
 
 class AboutScreen extends StatefulWidget {
   const AboutScreen({super.key});
@@ -12,87 +17,124 @@ class AboutScreen extends StatefulWidget {
 }
 
 class _AboutScreen extends State<AboutScreen> {
+  ProfileController profileController = Get.find();
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(26),
-          color: blogContainerColor,
-        ),
-        height: 380,
-        width: 140,
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Container(
-              width: 323,
-              height: 127,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      width: 300,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(26),
+        color: blogContainerColor,
+      ),
+      padding: EdgeInsets.all(16),
+      child: Obx(
+        () => profileController.isLoading.value
+            ? Center(child: CircularProgressIndicator())
+            : ListView(
+                shrinkWrap: true,
                 children: [
                   TitleText(text: 'Biography'),
                   SizedBox(
                     height: 10,
                   ),
-                  Expanded(
-                    child: Text(
-                      'If you\’re here, you probably know about my situation. I\’m going to assume that you already know that for the last almost eight months I\’ve been fighting a very public battle that involves topics I didn\’t know much about until recently boundaries, consent.',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        color: Colors.white,
-                      ),
+                  Text(
+                    profileController.userProfile.value.biography ?? "",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
                     ),
                   ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TitleText(text: 'Education'),
+                          SizedBox(height: 10),
+                          Text(
+                            profileController.userProfile.value.education !=
+                                    null
+                                ? profileController.userProfile.value.education!
+                                    .educationalBackground
+                                : "",
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      const Spacer(),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TitleText(text: 'Age'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            profileController.userProfile.value.age.toString() +
+                                ' years',
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      const Spacer(),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TitleText(text: 'Sex'),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            profileController.userProfile.value.gender ?? "",
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TitleText(text: 'Interest'),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  UserInterest(profileController: profileController),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TitleText(text: 'Social'),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: profileController
+                          .userProfile.value.socialLink!.length,
+                      itemBuilder: (ctx, index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 2.0),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.language,
+                                color: profileGolden,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                profileController
+                                    .userProfile.value.socialLink![index],
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        );
+                      })
                 ],
               ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TitleText(text: 'Education'),
-                    SizedBox(height: 10),
-                    Text(
-                      'PhD in design',
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-                const Spacer(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TitleText(text: 'Age'),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      '31 years',
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-                const Spacer(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TitleText(text: 'Sex'),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'Male',
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
-        ),
       ),
     );
   }
