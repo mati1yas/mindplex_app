7bc36b35917ad8ea6613099998457894032e7651
mati1yas <matiyassseifu@gmail.com>
2023-11-21 13:42:01 +0300

    feat: create notification card for usability and modularisation purpose

---
+++ lib/notification/widgets/notification_card.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mindplex_app/profile/user_profile_displays/profile_page.dart';
+
+import '../../blogs/blogs_controller.dart';
+import '../../blogs/screens/blog_detail_page.dart';
+import '../../routes/app_routes.dart';
+
+class NotificationCard extends StatelessWidget {
+  const NotificationCard({
+    super.key,
+    required this.blogsController,
+    required this.index,
+    required this.pageType,
+  });
+
+  final BlogsController blogsController;
+  final int index;
+  final String pageType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+      child: Container(
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                Get.to(DetailsPage(
+                    index: index,
+                    details: blogsController.filteredBlogs[index]));
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          //  will be modified in detail .
+
+                          Get.toNamed(AppRoutes.profilePage, parameters: {
+                            "me": "notme",
+                            "username": blogsController
+                                    .filteredBlogs[index].authorUsername ??
+                                ""
+                          });
+                        },
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(blogsController
+                                  .filteredBlogs[index].authorAvatar ??
+                              ""),
+                          radius: 20,
+                          backgroundColor: Colors.black,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      if (pageType == "Mentions")
+                        Text(
+                          blogsController
+                                  .filteredBlogs[index].authorDisplayName ??
+                              "",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      Spacer(),
+                      Icon(
+                        Icons.more_horiz,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                  Text(
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                      blogsController.filteredBlogs[index].postTitle ?? ""),
+                  Text(
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      blogsController.filteredBlogs[index].overview ?? ""),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(blogsController
+                                        .filteredBlogs[index].thumbnailImage ??
+                                    ""))),
+                        height: 170,
+                        width: 400,
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 1, right: 8.0),
+                          child: Container(
+                              height: 60,
+                              width: 35,
+                              margin: EdgeInsets.only(left: 10, top: 0),
+                              decoration: const BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(5),
+                                    bottomRight: Radius.circular(5),
+                                  )),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.only(bottom: 10),
+                                    child: blogsController.filteredBlogs[index]
+                                                .postTypeFormat ==
+                                            "text"
+                                        ? const Icon(
+                                            Icons.description_outlined,
+                                            color: Color(0xFF8aa7da),
+                                            size: 20,
+                                          )
+                                        : blogsController.filteredBlogs[index]
+                                                    .postTypeFormat ==
+                                                "video"
+                                            ? const Icon(
+                                                Icons.videocam,
+                                                color: Color.fromARGB(
+                                                    255, 185, 127, 127),
+                                                size: 20,
+                                              )
+                                            : const Icon(
+                                                Icons.headphones,
+                                                color: Colors.green,
+                                                size: 20,
+                                              ),
+                                  )
+                                ],
+                              )),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                ],
+              ),
+            ),
+            Divider(
+              color: Colors.white,
+              thickness: 1,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
