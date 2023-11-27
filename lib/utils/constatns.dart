827e80a5175f47fd2646b19d5a701c642fa4e819
mati1yas <matiyassseifu@gmail.com>
2023-11-27 15:08:29 +0300

    feat: declared global scaffold  key state

--- lib/utils/constatns.dart
+++ lib/utils/constatns.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class AppUrls {
   static var baseUrl = "https://staging.mindplex.ai/wp-json";
   static var loginUrl = '$baseUrl/auth/v1/token';
@@ -29,6 +31,11 @@ final List<String> emojis = [
   "🤮",
   "💩"
 ];
+
+class Keys {
+  static final globalkey = GlobalKey<ScaffoldState>();
+}
+
 List<String> countryList = [
   "Afghanistan",
   "Albania",
