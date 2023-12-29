6c92ffd61583aaf7cbd6064eed9164cb9dd08db9
natnael abebaw <natnaell70a@gmail.com>
2023-12-30 00:56:58 +0300

    add better error handling

--- lib/features/user_profile_displays/controllers/user_profile_controller.dart
+++ lib/features/user_profile_displays/controllers/user_profile_controller.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
@@ -9,6 +11,9 @@ import 'package:mindplex/features/user_profile_displays/services/profileServices
 import 'package:mindplex/features/user_profile_settings/models/user_profile.dart';
 import 'package:mindplex/services/api_services.dart';
 import 'package:mindplex/features/local_data_storage/local_storage.dart';
+import 'package:mindplex/utils/AppError.dart';
+import 'package:mindplex/utils/Toster.dart';
+import 'package:mindplex/utils/status.dart';
 
 import '../../authentication/controllers/auth_controller.dart';
 import '../../../utils/unkown_models/popularModel.dart';
@@ -154,17 +159,29 @@ class ProfileController extends GetxController {
   }
 
   RxList<Blog> publishedPosts = <Blog>[].obs;
-  RxBool isPostLoading = false.obs;
+  Rx<Status> status = Status.unknown.obs;
+  RxString errorMessage = "Something is very wrong!".obs;
+
   ProfileServices profileService = ProfileServices();
   Future<void> getPublishedPosts({required String username}) async {
     if (publishedPosts.length > 0) {
       return;
     }
+    status(Status.loading);
+    try {
+      List<Blog> res =
+          await profileService.getPublishedPosts(username: username);
+      publishedPosts.value = res;
+      status(Status.success);
+    } catch (e) {
+      status(Status.error);
 
-    isPostLoading.value = true;
-    List<Blog> res = await profileService.getPublishedPosts(username: username);
+      if (e is AppError) {
+        print("ok");
+        errorMessage(e.message);
+      }
 
-    publishedPosts.value = res;
-    isPostLoading.value = false;
+      Toster(message: errorMessage.value);
+    }
   }
 }
--- lib/features/user_profile_displays/view/screens/bookmark_screen.dart
+++ lib/features/user_profile_displays/view/screens/bookmark_screen.dart
@@ -1,81 +1,33 @@
-import 'dart:convert';
-
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import '../../controllers/user_profile_controller.dart';
-import 'package:mindplex/utils/colors.dart';
+import "../widgets/blog-widget.dart";
+import "../../../../utils/status.dart";
 
 class BookmarkScreen extends StatelessWidget {
   BookmarkScreen({super.key});
 
-  ProfileController profileController = Get.put(ProfileController());
+  final ProfileController profileController = Get.put(ProfileController());
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Obx(() => profileController.isPostLoading.value
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.separated(
-                itemCount: profileController.publishedPosts.length,
-                separatorBuilder: (context, index) => SizedBox(height: 10),
-                itemBuilder: (context, index) {
-                  var current = profileController.publishedPosts[index];
-                  return Container(
-                    padding: EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(26),
-                      color: blogContainerColor,
-                    ),
-                    height: 250,
-                    width: 140,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          current.publishedAt ?? "",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          current.postTitle ?? "",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 97, 255, 213),
-                            fontSize: 15,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Expanded(
-                            child: Text(
-                          current.overview ?? "",
-                          style: TextStyle(color: Colors.white),
-                        )),
-                        Row(
-                          children: [
-                            Text(
-                              current.minToRead ?? "",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              "${current.views.toString()}k views" ?? "",
-                              style: TextStyle(color: Colors.white),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  );
-                })));
+    return Container(child: Obx(() {
+      return profileController.status == Status.loading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : profileController.status == Status.error
+              ? Center(
+                  child: Text(profileController.errorMessage.value),
+                )
+              : ListView.separated(
+                  itemCount: profileController.publishedPosts.length,
+                  separatorBuilder: (context, index) => SizedBox(height: 10),
+                  itemBuilder: (context, index) {
+                    return BlogWidget(
+                        publishedPost: profileController.publishedPosts[index]);
+                  });
+    }));
   }
 }
