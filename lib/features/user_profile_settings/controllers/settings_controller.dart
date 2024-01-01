5c40460313722a2fbfeea2ac7197a4a7af9dbd91
abrsh-10 <abrhamsisay33@gmail.com>
2024-01-01 12:49:10 +0300

    input box widget extracted from general settings page and getx controller added

---
+++ lib/features/user_profile_settings/controllers/settings_controller.dart
@@ -0,0 +1,36 @@
+import 'package:get/get.dart';
+import 'package:mindplex/features/user_profile_settings/models/user_profile.dart';
+import 'package:mindplex/features/user_profile_settings/services/settings_api_service.dart';
+
+class SettingsController extends GetxController{
+  RxBool isLoading = true.obs;
+  RxString username = "".obs;
+  RxString? usernameError = "".obs;
+  RxString email = "".obs;
+  RxString? emailError = "".obs;
+  RxBool isSaved = false.obs;
+
+
+  final apiService = SettingsApiService().obs;
+
+  void fetchUserInfo(String name) async {
+    isLoading.value = true;
+     print(name);
+    UserProfile res = await apiService.value.fetchUserProfile(userName: name);
+    print(res.username);
+    username.value = res.username??"abr";
+    isLoading.value = false;
+    update();
+  }
+
+  RxString? setInputError(String inputLabel){
+    if(inputLabel == "Username"){
+      usernameError =  "Please enter your user name (Minimum of 3 characters)".obs;
+      return usernameError;
+    }
+    else if(inputLabel == "Email"){
+      emailError =  "Please enter a valid email address (ex. abc@gmail.com)".obs;
+      return emailError;
+    }
+  }
+}
--- lib/features/user_profile_settings/view/screens/general_settings.dart
+++ lib/features/user_profile_settings/view/screens/general_settings.dart
@@ -5,11 +5,13 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:intl/intl.dart';
 import 'package:mindplex/features/user_profile_displays/controllers/user_profile_controller.dart';
+import 'package:mindplex/features/user_profile_settings/controllers/settings_controller.dart';
 import 'package:mindplex/routes/app_routes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../utils/colors.dart';
 import '../../../authentication/controllers/auth_controller.dart';
+import '../widgets/input_box_widget.dart';
 
 class GeneralSettings extends StatefulWidget {
   const GeneralSettings({Key? key}) : super(key: key);
@@ -19,27 +21,27 @@ class GeneralSettings extends StatefulWidget {
 }
 
 final _formKey = GlobalKey<FormState>();
-String? user_name, user_email, user_display_name;
 String? userNameError, userEmailError, userDisplayNameError;
 
 class _GeneralSettingsState extends State<GeneralSettings> {
-  String? title;
-  bool isSaved = false;
-  bool isValueSet = false;
-  AuthController authController = Get.put(AuthController());
 
+  @override
+  void initState() {
+    super.initState();
+    profileController.getAuthenticatedUser();
+    userNameController.text = profileController.authenticatedUser.value.username??"";
+    emailController.text = profileController.authenticatedUser.value.userEmail??"";
+  }
+
+  TextEditingController userNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+
+  AuthController authController = Get.put(AuthController());
   ProfileController profileController = Get.put(ProfileController());
+  SettingsController settingsController = Get.find<SettingsController>();
 
   @override
   Widget build(BuildContext context) {
-    final userName = profileController.authenticatedUser.value.username ?? " ";
-    final email = profileController.authenticatedUser.value.userEmail ?? " ";
-    user_email = profileController.authenticatedUser.value.userEmail ?? " ";
-    TextEditingController uName = TextEditingController();
-    uName.text = userName;
-
-    user_name = uName.text;
-    user_display_name = uName.text;
 
     return Scaffold(
       backgroundColor: mainBackgroundColor,
@@ -52,29 +54,15 @@ class _GeneralSettingsState extends State<GeneralSettings> {
                 autovalidateMode: AutovalidateMode.always,
                 child: Column(children: [
                   const SizedBox(height: 10),
-                  _container(context, false, uName, null, TextInputType.name,
-                      userName, "uName", (() {})),
-                  userNameError != null && isSaved
-                      ? errorMessage(userNameError.toString())
-                      : Container(),
-                  _container(context, false, null, email, TextInputType.name,
-                      email, "uEmail", (() {})),
-                  userEmailError != null && isSaved
-                      ? errorMessage(userEmailError.toString())
-                      : Container(),
-                  _container(
-                    context,
-                    true,
-                    uName,
-                    null,
-                    TextInputType.name,
-                    userName,
-                    "uDisplayName",
-                    (() {}),
-                  ),
-                  userDisplayNameError != null && isSaved
-                      ? errorMessage(userDisplayNameError.toString())
-                      : Container(),
+                  inputBoxWithLabel(context, false, userNameController, TextInputType.name, "Username"),
+                  Obx(() => settingsController.usernameError != null && settingsController.isSaved.value
+                      ? errorMessage(settingsController.usernameError!.value)
+                      : Container(),),
+                  inputBoxWithLabel(context, false, emailController, TextInputType.name, "Email"),
+                  Obx(() => settingsController.emailError != null && settingsController.isSaved.value
+                      ? errorMessage(settingsController.emailError!.value)
+                      : Container(),),
+                  inputBoxWithLabel(context, true, userNameController, TextInputType.name, "Mindplex Handle"),
                 ])),
           ),
           Padding(
@@ -87,17 +75,10 @@ class _GeneralSettingsState extends State<GeneralSettings> {
                 Padding(
                     padding: const EdgeInsets.only(left: 15.0),
                     child: buildButton("Save", (() async {
-                      isSaved = false;
+                      settingsController.isSaved.value = false;
                       final isValidForm = _formKey.currentState!.validate();
-                      setState(() {
-                        isSaved = true;
-                      });
+                        settingsController.isSaved.value = true;
                       if (isValidForm) {
-                        print(user_name! +
-                            " " +
-                            user_display_name! +
-                            " " +
-                            user_email!);
                       }
                     }), Colors.blueAccent.shade200, true))
               ],
@@ -105,142 +86,6 @@ class _GeneralSettingsState extends State<GeneralSettings> {
           ),
         ]),
       ),
-    );
-  }
-
-  String? hintText(String? inputType) {
-    if (inputType == "uName") {
-      return "Username";
-    } else if (inputType == "uEmail") {
-      return "Email";
-    } else if (inputType == "uDisplayName") {
-      return "Mindplex handle";
-    }
-    return null;
-  }
-
-  Widget _container(
-      BuildContext context,
-      bool readOnly,
-      TextEditingController? controller,
-      String? initialValue,
-      TextInputType? inputType,
-      String? value,
-      String? type,
-      VoidCallback onTap) {
-    TextTheme textTheme = Theme.of(context).textTheme;
-    Color secondbackgroundColor = Theme.of(context).cardColor;
-    IconThemeData icontheme = Theme.of(context).iconTheme;
-
-    return Column(
-      children: [
-        const SizedBox(height: 20),
-        Container(
-          alignment: Alignment.topLeft,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              hintText(type) ?? " ",
-              style: TextStyle(
-                  color: Colors.amber,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20),
-            ),
-          ),
-        ),
-        const SizedBox(height: 5),
-        Container(
-            decoration: BoxDecoration(
-              color: secondbackgroundColor,
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 10,
-                  offset: const Offset(1, 1),
-                  color: const Color.fromARGB(54, 188, 187, 187),
-                )
-              ],
-              borderRadius: BorderRadius.circular(15),
-            ),
-            alignment: Alignment.center,
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: TextFormField(
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    readOnly: readOnly,
-                    controller: controller,
-                    initialValue: initialValue,
-                    keyboardType: inputType,
-                    style: textTheme.headline2?.copyWith(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.white),
-                    textAlignVertical: TextAlignVertical.center,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: mainBackgroundColor,
-                      errorBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Colors.red),
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      errorStyle: const TextStyle(fontSize: 0.01),
-                      contentPadding:
-                          const EdgeInsets.only(left: 25, top: 10, bottom: 10),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.amber, width: 2.0),
-                          borderRadius: BorderRadius.circular(15)),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Colors.blue),
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                    ),
-                    onTap: onTap,
-                    onChanged: (value) {
-                      if (type == "uName") {
-                        user_name = value;
-                        user_display_name = value;
-                        controller?.text = value;
-                      } else if (type == "uEmail") {
-                        user_email = value;
-                      }
-                    },
-                    validator: ((value) {
-                      if (type == "uName") {
-                        if (value != null && value.length < 3) {
-                          userNameError =
-                              "Please enter your user name (Minimum of 3 characters)";
-                          return userNameError;
-                        } else {
-                          userNameError = null;
-                          return null;
-                        }
-                      } else if (type == "uEmail") {
-                        final emailRegex = RegExp(
-                            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
-                        if (value!.isEmpty) {
-                          userEmailError =
-                              "Please enter a valid email address (ex. abc@gmail.com)";
-                          return userEmailError;
-                        } else if (emailRegex.hasMatch(value) == false) {
-                          userEmailError =
-                              "Please enter a valid email address (ex. abc@gmail.com)";
-                          return userEmailError;
-                        }
-                        userEmailError = null;
-                        return null;
-                      } else if (type == "uDisplayName") {
-                        if (value != null && value.length < 3) {
-                          userDisplayNameError =
-                              "Please enter your mindplex handle (Minimum of 3 characters)";
-                          return userDisplayNameError;
-                        } else {
-                          userDisplayNameError = null;
-                          return null;
-                        }
-                      }
-                      return null;
-                    })))),
-      ],
     );
   }
 }
@@ -282,17 +127,4 @@ Widget errorMessage(String? error) {
         error.toString(),
         style: const TextStyle(color: Colors.red),
       ));
-}
-
-snackbar(Text title, Text message) {
-  return Get.snackbar("", "",
-      snackStyle: SnackStyle.FLOATING,
-      snackPosition: SnackPosition.BOTTOM,
-      borderWidth: 2,
-      dismissDirection: DismissDirection.horizontal,
-      duration: const Duration(seconds: 4),
-      backgroundColor: Colors.blue,
-      titleText: title,
-      messageText: message,
-      margin: const EdgeInsets.only(top: 12, left: 15, right: 15, bottom: 15));
 }
