e356a06c107f399581bc7a5f21d11e1abac5d1c0
mati1yas <matiyassseifu@gmail.com>
2024-01-05 16:19:26 +0300

    feat: create new widgets for refactoring

---
+++ lib/features/user_profile_displays/view/widgets/user_profile_image_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../controllers/user_profile_controller.dart';
+
+class UserProfileImage extends StatelessWidget {
+  const UserProfileImage({
+    super.key,
+    required this.context,
+    required this.profileController,
+    required this.me,
+  });
+
+  final BuildContext context;
+  final ProfileController profileController;
+  final bool me;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: Colors.green,
+      radius: MediaQuery.of(context).size.width * 0.25,
+      backgroundImage: NetworkImage(
+        me
+            ? profileController.authenticatedUser.value.image ??
+                "assets/images/profile.PNG"
+            : profileController.userProfile.value.avatarUrl ??
+                "assets/images/profile.PNG",
+      ),
+    );
+  }
+}
---
+++ lib/features/user_profile_displays/view/widgets/user_profile_statistics_widget.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:mindplex/features/user_profile_displays/controllers/user_profile_controller.dart';
+
+class UserProfileStatistics extends StatelessWidget {
+  const UserProfileStatistics({
+    super.key,
+    required this.profileController,
+  });
+  final ProfileController profileController;
+
+  @override
+  Widget build(BuildContext context) {
+    var statistics = [
+      {
+        "amount": profileController.userProfile.value.friends.toString(),
+        "value": "Friends"
+      },
+      {
+        "amount": profileController.userProfile.value.followings.toString(),
+        "value": "Following"
+      },
+      {
+        "amount": profileController.userProfile.value.followers.toString(),
+        "value": "Followers"
+      },
+      {
+        "amount": profileController.userProfile.value.mpxr.toString(),
+        "value": " MPXR"
+      }
+    ];
+    return Container(
+      height: 46,
+      margin: const EdgeInsets.only(bottom: 18),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          for (var item in statistics)
+            Row(
+              children: [
+                statistics.indexOf(item) != statistics.length - 1
+                    ? Column(
+                        children: [
+                          Text(
+                            item['amount'].toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Text(
+                            item['value'].toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 190, 190, 190),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      )
+                    : Column(
+                        children: [
+                          Icon(
+                            Icons.warning,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "MPXR",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 190, 190, 190),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          )
+                        ],
+                      ),
+                if (statistics.indexOf(item) != statistics.length - 1)
+                  Container(
+                    height: 40.0,
+                    width: 1.0,
+                    color: const Color.fromARGB(255, 73, 150, 154),
+                    margin: EdgeInsets.symmetric(horizontal: 8.0),
+                  ), //
+              ],
+            ),
+        ],
+      ),
+    );
+    ;
+  }
+}
