65b0bac0cdf313c323732cf1f7c0b79fdc42554e
primequantuM4 <gediezra@gmail.com>
2024-01-05 12:37:36 +0300

    fix: top user icon

--- lib/features/drawer/view/widgets/top_user_profile_icon.dart
+++ lib/features/drawer/view/widgets/top_user_profile_icon.dart
@@ -19,19 +19,13 @@ class TopUserProfileIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => {Keys.globalkey.currentState!.openDrawer()},
-// child: Obx(() => CircleAvatar(
-      //       radius: 27,
-      //       backgroundImage: NetworkImage(authController.isGuestUser.value
-      //           ? authController.guestUserImage.value
-      //           : profileController.authenticatedUser.value.image ??
-      //               "https://secure.gravatar.com/avatar/44cb6ed8fa0451a09a6387dc8bf2533a?s=260&d=mm&r=g"),
-      //     )),
-      child: CircleAvatar(
-        radius: 27,
-        backgroundImage: NetworkImage(
-          "https://secure.gravatar.com/avatar/44cb6ed8fa0451a09a6387dc8bf2533a?s=260&d=mm&r=g",
-        ),
-      ),
+      child: Obx(() => CircleAvatar(
+            radius: 27,
+            backgroundImage: NetworkImage(authController.isGuestUser.value
+                ? authController.guestUserImage.value
+                : profileController.authenticatedUser.value.image ??
+                    "https://secure.gravatar.com/avatar/44cb6ed8fa0451a09a6387dc8bf2533a?s=260&d=mm&r=g"),
+          )),
     );
   }
 }
