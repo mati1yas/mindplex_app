9b71ca795f1e0218867adf5f71fc9456cec77101
mati1yas <matiyassseifu@gmail.com>
2024-01-22 15:18:38 +0300

    feat: link drawer controller with other draft related controller for managing state related to post drafting

--- lib/features/drawer/controller/drawer_controller.dart
+++ lib/features/drawer/controller/drawer_controller.dart
@@ -1,15 +1,20 @@
 import 'package:get/get.dart';
 import 'package:mindplex/features/authentication/controllers/auth_controller.dart';
 import 'package:mindplex/features/blogs/controllers/blogs_controller.dart';
+import 'package:mindplex/features/blogs/models/blog_model.dart';
 import 'package:mindplex/features/bottom_navigation_bar/controllers/bottom_page_navigation_controller.dart';
 import 'package:mindplex/features/drawer/model/drawer_model.dart';
 import 'package:mindplex/features/drawer/model/drawer_types.dart';
+import 'package:mindplex/features/user_profile_displays/controllers/DraftedPostsController.dart';
 import 'package:mindplex/routes/app_routes.dart';
 import 'package:mindplex/utils/colors.dart';
 
 class DrawerButtonController extends GetxController {
   BlogsController _blogsController = Get.find();
+  DraftedPostsController draftedPostsController = Get.find();
+
   PageNavigationController _pageNavigationController = Get.find();
+
   AuthController _authController = Get.find();
 
   // Have a list that handles pages that hasn't been implemented yet
@@ -49,17 +54,20 @@ class DrawerButtonController extends GetxController {
     } else if (drawerModel.pageName == AppRoutes.landingPage) {
       Get.back();
       _loadContents(drawerModel.postType!, drawerModel.postFormat!);
+
       _pageNavigationController.navigatePage(0);
     } else if (_authController.checkUserPrivellege(
       requiresPrivilege: drawerModel.requiresPrivilege,
     )) {
       Get.back();
+
       Get.toNamed(drawerModel.pageName, parameters: drawerModel.parameters);
     }
     currentDrawerType.value = drawerModel.drawerType;
   }
 
   _loadContents(String postType, String postFormat) {
+    draftedPostsController.resetDrafting();
     _blogsController.loadContents(postType, postFormat);
   }
 }
