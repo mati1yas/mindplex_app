96bb649d99c1e70e6d112921a5e5afc68edc2cc0
mati1yas <matiyassseifu@gmail.com>
2024-02-21 11:24:22 +0300

    feat: implement logic for fetching user profilempxr

---
+++ lib/features/user_profile_displays/models/user_profile_reputation_model.dart
@@ -0,0 +1,66 @@
+class UserProfileReputation {
+  int? user;
+  double? mpxr;
+  double? voteRepAvailabe;
+  String? publicAddress;
+  double? sharedRep;
+  List<Categories>? categories;
+  double? userReadTime;
+
+  UserProfileReputation(
+      {this.user,
+      this.mpxr,
+      this.voteRepAvailabe,
+      this.publicAddress,
+      this.sharedRep,
+      this.categories,
+      this.userReadTime});
+
+  UserProfileReputation.fromJson(Map<String, dynamic> json) {
+    user = json['user'];
+    mpxr = json['mpxr'];
+    voteRepAvailabe = json['vote_rep_availabe'];
+    publicAddress = json['public_address'];
+    sharedRep = json['shared_rep'];
+    if (json['categories'] != null) {
+      categories = <Categories>[];
+      json['categories'].forEach((v) {
+        categories!.add(new Categories.fromJson(v));
+      });
+    }
+    userReadTime = json['user_read_time'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['user'] = this.user;
+    data['mpxr'] = this.mpxr;
+    data['vote_rep_availabe'] = this.voteRepAvailabe;
+    data['public_address'] = this.publicAddress;
+    data['shared_rep'] = this.sharedRep;
+    if (this.categories != null) {
+      data['categories'] = this.categories!.map((v) => v.toJson()).toList();
+    }
+    data['user_read_time'] = this.userReadTime;
+    return data;
+  }
+}
+
+class Categories {
+  String? category;
+  double? rep;
+
+  Categories({this.category, this.rep});
+
+  Categories.fromJson(Map<String, dynamic> json) {
+    category = json['category'];
+    rep = json['rep'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['category'] = this.category;
+    data['rep'] = this.rep;
+    return data;
+  }
+}
