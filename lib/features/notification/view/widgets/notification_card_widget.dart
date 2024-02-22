bc3b9742a422d011e713e32b288067d744066dec
mati1yas <matiyassseifu@gmail.com>
2024-02-23 00:56:17 +0300

    fix : make notification content similar to website

--- lib/features/notification/view/widgets/notification_card_widget.dart
+++ lib/features/notification/view/widgets/notification_card_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:mindplex/features/blogs/controllers/blogs_controller.dart';
+import 'package:mindplex/utils/colors.dart';
+import 'package:mindplex/utils/constatns.dart';
 
 import '../../models/notification_model.dart';
 import '../../../../routes/app_routes.dart';
@@ -11,15 +13,21 @@ class NotificationCard extends StatelessWidget {
 
   final NotificationModel notification;
 
-  String buildReactionType(String type) {
+  String buildReactionType(
+    String type,
+    String message,
+  ) {
     final reactionTypePhrase = {
       "friendship_req": "has sent you a friend request",
-      "content_like": "liked your post",
+      "content_like": "liked your podcast",
       "follow": "started following you",
-      "post_comment": "commented on your post",
-      "content_react": "reacted to your post",
-      "content_share": "shared your post",
-      "moderator_approved": "Your post has been approved by a moderator"
+      "post_comment": "commented on your podcast",
+      "content_react": "reacted to your podcast",
+      "content_share": message,
+      "moderator_approved":
+          "Your requested content has been approved by a moderator",
+      "publish": "published a new $message",
+      "claim_start": "claimed your Request"
     };
     return reactionTypePhrase[type] ?? type;
   }
@@ -62,16 +70,16 @@ class NotificationCard extends StatelessWidget {
                     children: [
                       Row(
                         children: [
-                          Flexible(
+                          Expanded(
                             child: Text(
-                              '${notification.firstName! + " " + notification.lastName! + " " + buildReactionType(notification.type!)}',
+                              '${notification.firstName! + " " + notification.lastName! + " " + buildReactionType(notification.type ?? " ", notification.message ?? " ")}',
                               style: TextStyle(
-                                color: Colors.white,
+                                color: bodyTextColor,
                                 fontSize: 18,
                                 fontWeight: FontWeight.w400,
                               ),
                             ),
-                          )
+                          ),
                         ],
                       ),
                       Text(
