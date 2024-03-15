57a00bf4ed7cde40a68a336b1ad854b3f63ab8ab
mati1yas <matiyassseifu@gmail.com>
2024-03-15 11:31:56 +0300

    fix : make the card take different messages for different errors

--- lib/utils/no_internet_card_widget.dart
+++ lib/utils/no_internet_card_widget.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-Widget noInternetCard(VoidCallback onRefresh){
+Widget noInternetCard(VoidCallback onRefresh, String? message) {
   return Container(
-    width: MediaQuery.of(Get.context!).size.width *0.65,
+    width: MediaQuery.of(Get.context!).size.width * 0.65,
     height: 150,
     margin: EdgeInsets.all(15.0),
     padding: EdgeInsets.all(15),
     decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(10),
-      color: Colors.white70
-    ),
+        borderRadius: BorderRadius.circular(10), color: Colors.white70),
     child: Column(
       children: [
         GestureDetector(
           onTap: onRefresh,
-          child: Icon(Icons.refresh_outlined,size: 35,),
+          child: Icon(
+            Icons.refresh_outlined,
+            size: 35,
+            color: Colors.red,
+          ),
         ),
-        Text("Looks like you have a problem with your internet connection",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 17),)
+        Text(
+          message ??
+              "Looks like you have a problem with your internet connection",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+        )
       ],
     ),
   );
