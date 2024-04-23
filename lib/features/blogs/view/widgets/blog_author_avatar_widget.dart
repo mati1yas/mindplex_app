f32e8bc534ec87a9a8515c27c25f6213b7382540
mati1yas <matiyassseifu@gmail.com>
2024-04-23 11:43:46 +0300

    feat: refactored author avatar for a better usability

---
+++ lib/features/blogs/view/widgets/blog_author_avatar_widget.dart
@@ -0,0 +1,32 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class BlogAuthorAvatarWidget extends StatelessWidget {
+  const BlogAuthorAvatarWidget({
+    super.key,
+    required this.imageUrl,
+    required this.radius,
+  });
+
+  final String imageUrl;
+  final double radius;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(25),
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          placeholder: (context, url) =>
+              Image.asset('assets/images/user_avatar.png'),
+          errorWidget: (context, url, error) =>
+              Image.asset('assets/images/user_avatar.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      radius: radius,
+      backgroundColor: Colors.white,
+    );
+  }
+}
