98cdf41099778514b1184410270ee16aae3076a6
mati1yas <matiyassseifu@gmail.com>
2024-04-23 11:43:02 +0300

    feat: add read more and read less feature to authors bio .   make author avatar cach be displayed from cache network image

--- lib/features/blogs/view/widgets/author_tile_widget.dart
+++ lib/features/blogs/view/widgets/author_tile_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
@@ -12,6 +13,7 @@ import 'package:mindplex/features/user_profile_displays/controllers/user_profile
 import 'package:mindplex/routes/app_routes.dart';
 import 'package:mindplex/utils/colors.dart';
 import 'package:mindplex/utils/double_to_string_convertor.dart';
+import 'package:readmore/readmore.dart';
 
 class AuthorsTileWidget extends StatelessWidget {
   const AuthorsTileWidget({
@@ -37,141 +39,148 @@ class AuthorsTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         GestureDetector(
           onTap: () {
             openAuthorProfile(details.authors![authorIndex].username ?? "",
                 context, authController);
           },
-          child: Container(
-            margin: EdgeInsets.all(20),
-            child: CircleAvatar(
-              radius: 21,
-              backgroundImage: NetworkImage(
-                  details.authors![authorIndex].avatar ?? "" ?? ""),
-            ),
-          ),
-        ),
-        InkWell(
-          onTap: () {
-            openAuthorProfile(details.authors![authorIndex].username ?? "",
-                context, authController);
-          },
-          child: Container(
-            width: MediaQuery.of(context).size.width * .40,
-            margin: EdgeInsets.only(right: 3, top: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  details.authors![authorIndex].displayName ?? "",
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  style: const TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.normal,
-                      color: titleTextColor),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.all(20),
+                child: CircleAvatar(
+                  radius: 21,
+                  backgroundImage:
+                      NetworkImage(details.authors![authorIndex].avatar ?? ""),
                 ),
-                SizedBox(height: 5),
-                Obx(
-                  () => blogTimeSpentController.isLoadinAuthorsReputation.value
-                      ? Container(
-                          width: 10,
-                          height: 10,
-                          child: CircularProgressIndicator(
-                            color: Colors.green,
-                          ),
-                        )
-                      : Text(
-                          details.authors![authorIndex].mpxr != null &&
-                                  details.authors![authorIndex].mpxr!.value !=
-                                      null
-                              ? numberToString(
-                                      numberValue: details
-                                          .authors![authorIndex].mpxr!.value,
-                                      decimalPlace: 5) +
-                                  " MPXR "
-                              : " - MPXR",
-                          // authorIndex == 0
-                          //     ? details.reputation.value != null
-                          //         ? details.reputation.value!.author!.mpxr!
-                          //             .toStringAsFixed(5)
-                          //         : " - " + " MPXR"
-                          //     : " - ",
-                          style: TextStyle(
-                              color: bodyTextColor,
-                              fontWeight: FontWeight.w500),
-                        ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  parse(details.authors![authorIndex].bio)
-                      .documentElement!
-                      .text,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  style: TextStyle(
-                      color: bodyTextColor, fontWeight: FontWeight.w300),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Spacer(),
-        Obx(() => profileController.authenticatedUser.value.username ==
-                details.authorUsername
-            ? SizedBox()
-            : GestureDetector(
-                onTap: () {
-                  if (authController.isGuestUser.value) {
-                    authController.guestReminder(context);
-                  } else if (!likeDislikeConroller
-                      .isSendingFollowRequest.value) {
-                    likeDislikeConroller.followUnfollowBlogAuthor(
-                        blog: details,
-                        blogIndex: blogIndex,
-                        authorIndex: authorIndex,
-                        userName: details.authors![authorIndex].username!,
-                        isFollowing:
-                            details.authors![authorIndex].isFollowing!.value);
-                  }
-                },
-                child: Obx(
-                  () => Container(
-                      padding: const EdgeInsets.only(
-                          top: 6, left: 20, right: 20, bottom: 6),
-                      margin: EdgeInsets.only(top: 15),
-                      decoration: const BoxDecoration(
-                          color: Color.fromARGB(235, 41, 92, 120),
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      child: likeDislikeConroller
-                                  .isSendingFollowRequest.value &&
-                              likeDislikeConroller.clickedAuthorIndex ==
-                                  authorIndex
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * .40,
+                margin: EdgeInsets.only(right: 3, top: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      details.authors![authorIndex].displayName ?? "",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: const TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.normal,
+                          color: titleTextColor),
+                    ),
+                    SizedBox(height: 5),
+                    Obx(
+                      () => blogTimeSpentController
+                              .isLoadinAuthorsReputation.value
                           ? Container(
-                              height: 24,
-                              width: 24,
+                              width: 10,
+                              height: 10,
                               child: CircularProgressIndicator(
                                 color: Colors.green,
-                              ))
-                          : Obx(
-                              () => Text(
-                                details.authors![authorIndex].isFollowing!.value
-                                    ? 'Unfollow'
-                                    : 'Follow',
-                                style: TextStyle(
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.w200,
-                                    color: Colors.white),
                               ),
-                            )),
+                            )
+                          : Text(
+                              details.authors![authorIndex].mpxr != null &&
+                                      details.authors![authorIndex].mpxr!
+                                              .value !=
+                                          null
+                                  ? numberToString(
+                                          numberValue: details
+                                              .authors![authorIndex]
+                                              .mpxr!
+                                              .value,
+                                          decimalPlace: 5) +
+                                      " MPXR "
+                                  : " - MPXR",
+                              // authorIndex == 0
+                              //     ? details.reputation.value != null
+                              //         ? details.reputation.value!.author!.mpxr!
+                              //             .toStringAsFixed(5)
+                              //         : " - " + " MPXR"
+                              //     : " - ",
+                              style: TextStyle(
+                                  color: bodyTextColor,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                    ),
+                    SizedBox(height: 5),
+                  ],
                 ),
-              )),
-        SizedBox(
-          width: 16,
-        )
+              ),
+              Spacer(),
+              Obx(() => profileController.authenticatedUser.value.username ==
+                      details.authorUsername
+                  ? SizedBox()
+                  : GestureDetector(
+                      onTap: () {
+                        if (authController.isGuestUser.value) {
+                          authController.guestReminder(context);
+                        } else if (!likeDislikeConroller
+                            .isSendingFollowRequest.value) {
+                          likeDislikeConroller.followUnfollowBlogAuthor(
+                              blog: details,
+                              blogIndex: blogIndex,
+                              authorIndex: authorIndex,
+                              userName: details.authors![authorIndex].username!,
+                              isFollowing: details
+                                  .authors![authorIndex].isFollowing!.value);
+                        }
+                      },
+                      child: Obx(
+                        () => Container(
+                            padding: const EdgeInsets.only(
+                                top: 6, left: 20, right: 20, bottom: 6),
+                            margin: EdgeInsets.only(top: 20),
+                            decoration: const BoxDecoration(
+                                color: Color.fromARGB(235, 41, 92, 120),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: likeDislikeConroller
+                                        .isSendingFollowRequest.value &&
+                                    likeDislikeConroller.clickedAuthorIndex ==
+                                        authorIndex
+                                ? Container(
+                                    height: 24,
+                                    width: 24,
+                                    child: CircularProgressIndicator(
+                                      color: Colors.green,
+                                    ))
+                                : Obx(
+                                    () => Text(
+                                      details.authors![authorIndex].isFollowing!
+                                              .value
+                                          ? 'Unfollow'
+                                          : 'Follow',
+                                      style: TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.w200,
+                                          color: Colors.white),
+                                    ),
+                                  )),
+                      ),
+                    )),
+              SizedBox(
+                width: 16,
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: ReadMoreText(
+            parse(details.authors![authorIndex].bio).documentElement!.text,
+            trimMode: TrimMode.Line,
+            trimLines: 2,
+            style: TextStyle(color: bodyTextColor, fontWeight: FontWeight.w300),
+            colorClickableText: Color.fromARGB(255, 4, 187, 153),
+          ),
+        ),
       ],
     );
   }
