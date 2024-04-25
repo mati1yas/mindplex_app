76b9572de0b117629e7d546e21f855e7c26f58eb
mati1yas <matiyassseifu@gmail.com>
2024-04-25 10:06:27 +0300

    feat : set string constants for a better reusablity and maintenance

---
+++ lib/utils/snackbar_constants.dart
@@ -0,0 +1,23 @@
+class SnackBarConstantTitle {
+  static var failureTitle = "Oops !";
+  static var successTitle = "Congratulations";
+}
+
+class SnackBarConstantMessage {
+  static var draftDeleteSuccess = "Your draft has successfully been deleted ";
+  static var draftDeleteFailure =
+      "Failed to delete draft , please try again ! ";
+  static var draftUpdateSuccess = "Your draft has successfully been updated ";
+  static var draftUpdateFailure =
+      "Failed to updated draft , please try again ! ";
+  static var draftSaveSuccess = "Draft Saved Successfully";
+  static var draftSaveFailure = "Failed to save draft , please try again ! ";
+  static var socialPostSucccess = "Successfully posted on social feed";
+  static var socialPostFailure = "Failed to post , please try again ! ";
+
+  static var noInternetConnection =
+      "There seems to be a problem with your connection";
+
+  static var unKnowenError = "Something went wrong , Please try Again !";
+  static var mpxrLoadingFailure = "Failed To Load Mpxr values";
+}
